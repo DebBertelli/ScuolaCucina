5b07ead03f477bf2ae3ce7d7dcd60961a1ace7da
vikviper <vincenzo.maio@gmail.com>
2020-05-19 20:38:43 +0200

    pulizia codice

--- ScuolaCucina/src/it/begear/progetto/entity/Utenti.java
+++ ScuolaCucina/src/it/begear/progetto/entity/Utenti.java
@@ -1,14 +1,11 @@
 package it.begear.progetto.entity;
 
-
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,32 +17,23 @@ import lombok.NoArgsConstructor;
 @Data
 public class Utenti {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id_utente")
-	private int id;
-	
-	@Column(name = "nome_utente")
-	private String nome_utente;
-	
-	@Column(name="cognome_utente")
-	private String cognome_utente;
-	
-	@Column(name="email_utente")
-	private String email_utente;
-	
-	@Column(name="username")
-	private String username;
-	
-	@Column(name="psw")
-	private String psw;
-	
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id_utente")
+  private int id;
 
+  @Column(name = "nome_utente")
+  private String nome_utente;
 
+  @Column(name = "cognome_utente")
+  private String cognome_utente;
 
+  @Column(name = "email_utente")
+  private String email_utente;
 
-	
+  @Column(name = "username")
+  private String username;
 
-	
-	
+  @Column(name = "psw")
+  private String psw;
 }

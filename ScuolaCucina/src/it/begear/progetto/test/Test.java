db3f44524ee2144c296f8ef94733d07085368658
DebBertelli <deborah_bertelli@yahoo.com>
2020-05-20 15:19:22 +0200

    Sincronizzazione

--- ScuolaCucina/src/it/begear/progetto/test/Test.java
+++ ScuolaCucina/src/it/begear/progetto/test/Test.java
@@ -9,27 +9,37 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import it.begear.progetto.entity.Amministratori;
+import it.begear.progetto.entity.Docenti;
 import it.begear.progetto.entity.Utenti;
-
-
 
 public class Test {
 
 	public static void main(String[] args) {
-		
-		List<Utenti> listaUtenti = read();
+
+		/*List<Utenti> listaUtenti = read();
 		for(Utenti user: listaUtenti) {
 			System.out.println(user.toString());
+		}*/
+		
+		List<Docenti> listaDocenti = read2();
+		for(Docenti docente: listaDocenti) {
+			System.out.println(docente.toString());
+		}
+
+		List<Amministratori> listaAdmin = leggiAdmin();
+		for (Amministratori admin : listaAdmin) {
+			System.out.println(admin.toString());
 		}
 	}
-	
+
 	public static SessionFactory getSessionFactory() {
 		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
 		Metadata metadata = new MetadataSources(ssr).getMetadataBuilder().build();
 		SessionFactory sFactory = metadata.getSessionFactoryBuilder().build();
 		return sFactory;
 	}
-	
+
 	public static List<Utenti> read() {
 		Session session = getSessionFactory().openSession();
 		@SuppressWarnings("unchecked")
@@ -37,6 +47,16 @@ public class Test {
 		session.close();
 		System.out.println("Trovati " + utenti.size() + " utenti");
 		return utenti;
+
+	}
+
+	public static List<Amministratori> leggiAdmin() {
+		Session session = getSessionFactory().openSession();
+		@SuppressWarnings("unchecked")
+		List<Amministratori> admin = session.createQuery("FROM Amministratori").list();
+		session.close();
+		System.out.println("Trovati " + admin.size() + " amministratori");
+		return admin;
 
 	}
 

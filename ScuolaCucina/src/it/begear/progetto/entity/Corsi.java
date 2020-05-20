88563b33c746924fe5fc3558ce13a97f69d38504
camelia84 <57041859+camelia84@users.noreply.github.com>
2020-05-20 15:02:42 +0200

    new class pojo Corsi

---
+++ ScuolaCucina/src/it/begear/progetto/entity/Corsi.java
@@ -0,0 +1,40 @@
+package it.begear.progetto.entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Corsi {
+
+	@Id
+	  @GeneratedValue(strategy = GenerationType.IDENTITY)
+	  @Column(name = "id_corsi")
+	  private int id;
+	
+    @Column(name="titolo")
+	private String titolo;
+    
+    @Column(name="ore")
+	private int durata;
+    
+    @Column(name="id_docente")
+	private String docenti;
+    
+    @Column(name="max_partecipanti")
+	private int maxPartecipanti;
+    
+}
